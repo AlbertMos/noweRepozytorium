69865bc969d68df9fffb46bd002bd98bcc35b953
Albert <albertmossakowski@gmail.com>
2024-01-14 19:38:16 +0100

    Dodano

---
+++ calculator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+def kalkulator():
+    try:
+        liczba1 = float(input("Podaj pierwsza liczbe: "))
+        operator = input("Podaj operator (+, -, *, /): ")
+        liczba2 = float(input("Podaj druga liczbe: "))
+
+        if operator == '+':
+            wynik = liczba1 + liczba2
+        elif operator == '-':
+            wynik = liczba1 - liczba2
+        elif operator == '*':
+            wynik = liczba1 * liczba2
+        elif operator == '/':
+            wynik = liczba1 / liczba2
+        else:
+            print("Bledny wybor")
+            return
+
+        print("Wynik:",wynik)
+
+    except ValueError:
+        print("Blad")
+
+kalkulator()
